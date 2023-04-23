33ad6d6e56549923c97a9e68adcfe628a3ea3339
Maxim L <max.litwinowarriors@gmail.com>
2023-04-23 13:30:29 +0300

    upd

--- app/Handlers/WebhookHandler.php
+++ app/Handlers/WebhookHandler.php
@@ -5,20 +5,69 @@ namespace App\Handlers;
 use App\Helpers\TgHelper;
 use DefStudio\Telegraph\Handlers\WebhookHandler as TGWebhookHandler;
 use DefStudio\Telegraph\Models\TelegraphChat;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Illuminate\Support\Stringable;
 
 class WebhookHandler extends TGWebhookHandler
 {
     public function start(): void
     {
-        $this->chat->html("Start: {$this->chat->chat_id}")->send();
+        $preparedString = Str::of($this->data->get('text'))->remove('/start');
+
+        if ($preparedString->isNotEmpty()) {
+            $answer = $preparedString->explode('_')->whenNotEmpty(function ($value) {
+                $numbers = "{$value->get(0)} " . Str::replace('c', ',', $value->get(1)) .
+                    ' на ' . Str::replace('c', ',', $value->get(2)) . " {$value->get(3)}.";
+
+                return [
+                    'startText' => 'Вы создали заявку обмена ' . $numbers . ' Наш менеджер свяжется с вами в ближайшее время.',
+                    'adminText' => 'Запрос на обмен валюты по курсу - ' . $numbers
+                ];
+            });
+        } else {
+            $answer = [
+                'startText' => 'Вы создали заявку обмена. Наш менеджер свяжется с вами в ближайшее время.',
+                'adminText' => 'Пользователь пришел по ссылке без указания валют'
+            ];
+        }
+
+        /** Send to user */
+        $this->chat->html($answer['startText'])->send();
+
+        /** Send to admin */
+        $adminChat = TgHelper::findAdminChatId();
+
+        if (!TgHelper::isAdmin($this->chat->chat_id)) {
+            $adminChat->forwardMessage($this->chat->chat_id, $this->messageId)->send();
+            $adminChat->html($answer['adminText'])->send();
+        }
     }
 
     public function handleChatMessage(Stringable $text): void
     {
-        if (TgHelper::isAdmin($this->chat->id)) {
-            $adminChat = TelegraphChat::find($this->chat->id);
-            $adminChat->html("Received: $text")->send();
+        /**
+         * @var TelegraphChat $adminChat
+         */
+        $adminChat = TgHelper::findAdminChatId();
+
+        /** Forward to admin */
+        if (!TgHelper::isAdmin($this->chat->chat_id)) {
+            $adminChat->forwardMessage($this->chat->chat_id, $this->messageId)->send();
+        } else {
+            $forwardUserId = data_get($this->request->toArray(), 'message.reply_to_message.forward_from.id');
+
+            if (isset($forwardUserId)) {
+                $userChat = TelegraphChat::query()->where('chat_id', $forwardUserId)->first();
+
+                isset($userChat) ? $userChat->forwardMessage($adminChat, $this->messageId)->send()
+                    : $adminChat->html('Пользователя не существует в базе данных')->send();
+            } else {
+                $adminChat
+                    ->html('Для ответа клиенту используйте функцию "поделиться" на сообщении клиента')
+                    ->photo(storage_path('app/public/files/instraction.png'))
+                    ->send();
+            }
         }
     }
 }
--- app/Helpers/TgHelper.php
+++ app/Helpers/TgHelper.php
@@ -2,10 +2,22 @@
 
 namespace App\Helpers;
 
+use DefStudio\Telegraph\Models\TelegraphChat;
+
 class TgHelper
 {
     public static function isAdmin($chatId)
     {
         return !empty(env('ADMIN_CHAT_ID')) && env('ADMIN_CHAT_ID') == $chatId;
+    }
+
+    public static function getAdminChatId()
+    {
+        return env('ADMIN_CHAT_ID');
+    }
+
+    public static function findAdminChatId()
+    {
+        return TelegraphChat::query()->where('chat_id', TgHelper::getAdminChatId())->first();
     }
 }
