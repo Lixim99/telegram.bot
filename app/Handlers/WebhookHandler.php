c472bcc3657a6518039a6e5a550f4a50348bca97
Maxim Litvinov <max_litwinow@redcollar.ru>
2023-04-23 06:41:03 +0300

    update

--- app/Handlers/WebhookHandler.php
+++ app/Handlers/WebhookHandler.php
@@ -6,5 +6,8 @@ use DefStudio\Telegraph\Handlers\WebhookHandler as TGWebhookHandler;
 
 class WebhookHandler extends TGWebhookHandler
 {
-
+    public function start(): void
+    {
+        $this->chat->html("Start: {$this->chat->chat_id}")->send();
+    }
 }
