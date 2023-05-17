a036594cbf34e757bebdce173533234318704d51
Maxim Litvinov <max_litwinow@redcollar.ru>
2023-05-17 13:03:30 +0300

    upd

--- app/Handlers/WebhookHandler.php
+++ app/Handlers/WebhookHandler.php
@@ -21,13 +21,13 @@ class WebhookHandler extends TGWebhookHandler
                     ' на ' . Str::replace('c', ',', $value->get(2)) . " {$value->get(3)}.";
 
                 return [
-                    'startText' => 'Вы создали заявку обмена ' . $numbers . ' Наш менеджер свяжется с вами в ближайшее время.',
+                    'startText' => ' Я хочу обменять ' . $numbers . '.',
                     'adminText' => 'Запрос на обмен валюты по курсу - ' . $numbers
                 ];
             });
         } else {
             $answer = [
-                'startText' => 'Вы создали заявку обмена. Наш менеджер свяжется с вами в ближайшее время.',
+                'startText' => 'Вы создали заявку обмена.',
                 'adminText' => 'Пользователь пришел по ссылке без указания валют'
             ];
         }
