555722671a0712109dde988355608480b35f6cc6
Maxim Litvinov <max_litwinow@redcollar.ru>
2023-06-18 14:01:23 +0300

    test

---
+++ app/Http/Controllers/Front/HomepageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+
+class HomepageController extends Controller
+{
+//    public function __construct(HomepageRepository $repository)
+//    {
+//        $this->repository = $repository;
+//    }
+
+    public function show()
+    {
+        dd(23);
+    }
+}
